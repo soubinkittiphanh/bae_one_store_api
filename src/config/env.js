ac5b6f56a0591d1a40ecd2d3befe5b683122f6dd
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2023-09-04 10:38:07 +0700

    production api

--- src/config/env.js
+++ src/config/env.js
@@ -4,12 +4,12 @@ const dbClient = require("./dbClient").clientDB
 
 
 const config = {
-    port: process.env.PORT || 8080,
+    port: process.env.PORT || 8081,
     nodeEnv: process.env.NODE_ENV || "development",
     host: process.env.HOST || "localhost",
     actksecret: 'Jacke3848b9bd2e3eee522325953aafc118ed017c811cc93fae99a4b2f5ba3506e0e217636b3b509055900cb1da7594b0ce6c7192907213291818a4fdc89bf605ce8',
     rfTkSecret: 'Jacke3848b9bd2e3eee522325953aafc118ed017c811cc93fae99a4b2f5ba3506e0e217636b3b509055900cb1da7',
-    db: dbClient.uat1
+    db: dbClient.chithanh
 }
 
 module.exports = config;
