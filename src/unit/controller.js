63395d02787f5e6501657cd3c684210fd0c3c877
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2023-10-10 17:09:27 +0700

    migration v6

--- src/unit/controller.js
+++ src/unit/controller.js
@@ -7,6 +7,7 @@ const logger = require('../api/logger');
 exports.createUnitModel = async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
+    logger.error(`Validate data = false`)
     return res.status(400).json({ errors: errors.array() });
   }
 
@@ -24,6 +25,16 @@ exports.getUnitModels = async (req, res) => {
   try {
     // Find all UnitModels in the database
     const unitModels = await UnitModel.findAll();
+    res.status(200).json(unitModels);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+exports.getUnitActiveModels = async (req, res) => {
+  try {
+    // Find all UnitModels in the database
+    const unitModels = await UnitModel.findAll({where:{isActive:true}});
     res.status(200).json(unitModels);
   } catch (error) {
     console.error(error);
@@ -48,28 +59,46 @@ exports.getUnitModelById = async (req, res) => {
 };
 
 exports.updateUnitModel = async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-
   const { id } = req.params;
+  const { name, unitRate, isActive } = req.body;
   try {
-    // Find the UnitModel with the specified id and update its data
-    const [rowsAffected, [updatedUnitModel]] = await UnitModel.update(req.body, {
-      where: { id },
-      returning: true
-    });
-    if (rowsAffected === 1) {
-      res.status(200).json(updatedUnitModel);
-    } else {
-      res.status(404).json({ message: 'Unit model not found' });
+    const unit = await UnitModel.findByPk(id);
+    if (!unit) {
+      return res.status(404).json({ message: 'Unit not found' });
     }
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Internal server error' });
+    unit.name = name;
+    unit.unitRate = unitRate;
+    unit.isActive = isActive;
+    await unit.save();
+    res.json(unit);
+  } catch (err) {
+    logger.error(err);
+    res.status(500).json({ message: 'Something went wrong' });
   }
 };
+// exports.updateUnitModel = async (req, res) => {
+//   const errors = validationResult(req);
+//   if (!errors.isEmpty()) {
+//     return res.status(400).json({ errors: errors.array() });
+//   }
+
+//   const { id } = req.params;
+//   try {
+//     // Find the UnitModel with the specified id and update its data
+//     const [rowsAffected, [updatedUnitModel]] = await UnitModel.update(req.body, {
+//       where: { id },
+//       returning: true
+//     });
+//     if (rowsAffected === 1) {
+//       res.status(200).json(updatedUnitModel);
+//     } else {
+//       res.status(404).json({ message: 'Unit model not found' });
+//     }
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ message: 'Internal server error' });
+//   }
+// };
 
 exports.deleteUnitModel = async (req, res) => {
   const { id } = req.params;
