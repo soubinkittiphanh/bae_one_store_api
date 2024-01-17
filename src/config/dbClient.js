7c5db035f78c23137c9f9e8bde45f683ea85b6cd
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2024-01-17 13:45:58 +0700

    migration v6

--- src/config/dbClient.js
+++ src/config/dbClient.js
@@ -90,6 +90,13 @@ const clientDB = {
         "database": "dcommerce_pro_little_boutique",
         "port": 3306,
     },
+    "auto": {
+        "host": "150.95.31.23",
+        "user": "root",
+        "password": "sdat@3480",
+        "database": "dcommerce_pro_little_boutique",
+        "port": 3306,
+    },
 }
 
 module.exports = {
