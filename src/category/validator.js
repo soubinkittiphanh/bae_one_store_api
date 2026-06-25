a837ab8b747e5d92b3e4597280c260e593f93ea9
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2026-06-25 07:36:15 +0700

    UPDATE LATEST CATEGORY ISSUE FIX

--- src/category/validator.js
+++ src/category/validator.js
@@ -4,14 +4,14 @@ const createCategoryValidation = [
   body('categ_name').notEmpty().withMessage('Category name is required').isString(),
   body('categ_desc').optional({ nullable: true }).isString(),
   body('isActive').optional().isBoolean().withMessage('isActive must be a boolean'),
-  body('mainCategoryId').notEmpty().withMessage('Main Category ID is required').isInt().withMessage('Main Category ID must be an integer'),
+  //body('mainCategoryId').notEmpty().withMessage('Main Category ID is required').isInt().withMessage('Main Category ID must be an integer'),
 ];
 
 const updateCategoryValidation = [
   body('categ_name').optional().isString(),
   body('categ_desc').optional({ nullable: true }).isString(),
   body('isActive').optional().isBoolean().withMessage('isActive must be a boolean'),
-  body('mainCategoryId').optional().isInt().withMessage('Main Category ID must be an integer'),
+  //body('mainCategoryId').optional().isInt().withMessage('Main Category ID must be an integer'),
 ];
 
 module.exports = {
